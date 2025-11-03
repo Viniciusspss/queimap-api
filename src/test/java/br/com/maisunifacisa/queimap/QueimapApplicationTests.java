e687b4baf2c81b930b5589e48e0787953f4579ac
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-11-02 22:39:24 -0300

    Add global exception handler

---
+++ src/test/java/br/com/maisunifacisa/queimap/QueimapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maisunifacisa.queimap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueimapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
